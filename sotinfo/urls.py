a9189e9b48d913369b0ece4428793b13c297ef0c
Kshitij Moghe <kshitijmoghe10@gmail.com>
2025-03-06 00:56:34 +0530

    urls updated

--- sotinfo/urls.py
+++ sotinfo/urls.py
@@ -7,4 +7,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('jet/', include('jet.urls', 'jet')),
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)), ]
+    path('api/', include(router.urls)), 
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
