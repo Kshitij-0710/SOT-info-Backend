69443a37f035696bdbad3ac8cd8d32e3e4ac49c0
Kshitij Moghe <kshitijmoghe10@gmail.com>
2025-03-09 21:38:09 +0530

    unnececary files removed and the right app added

--- sotinfo/routing.py
+++ sotinfo/routing.py
@@ -2,13 +2,12 @@ from rest_framework.routers import DefaultRouter
 
 from authentication.views import AuthViewSet
 from rest_framework.routers import DefaultRouter
-from .views import AchievementViewSet, ProjectViewSet, ResearchViewSet
+from catogories.views import AchievementViewSet, ProjectViewSet, ResearchViewSet
 
 
 router = DefaultRouter() 
 
 router.register(r'auth', AuthViewSet, basename='auth')
-
-router.register(r'achievements', AchievementViewSet)
-router.register(r'projects', ProjectViewSet)
-router.register(r'research', ResearchViewSet)
+router.register(r'achievements', AchievementViewSet, basename='ach')
+router.register(r'projects', ProjectViewSet,basename='projects')
+router.register(r'research', ResearchViewSet,basename='res')
