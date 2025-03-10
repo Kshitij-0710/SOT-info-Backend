952cbdb5765ae3b31d1b757b3063e11d598db6d2
Kshitij Moghe <kshitijmoghe10@gmail.com>
2025-03-10 11:54:57 +0530

    settings.py updated

--- sotinfo/settings.py
+++ sotinfo/settings.py
@@ -73,13 +73,13 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
-
+load_dotenv()
 # Email settings (Using Gmail example)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-load_dotenv()
+
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
