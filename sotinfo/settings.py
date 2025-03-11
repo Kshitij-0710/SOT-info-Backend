65bc615b4e396d221d9f3db393c6f3bcd25ef0ce
Kshitij Moghe <kshitijmoghe10@gmail.com>
2025-03-11 11:23:10 +0530

    added load.env and updated superuser

--- sotinfo/settings.py
+++ sotinfo/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv()
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
