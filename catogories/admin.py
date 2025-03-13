de8afe2e5960a3dd0e7a84cd586925fefa38542d
Kshitij Moghe <kshitijmoghe10@gmail.com>
2025-03-13 11:21:11 +0530

    admin.py updated

--- catogories/admin.py
+++ catogories/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Form
 
-admin.site.register(Form)
+@admin.register(Form)
+class FormAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'user_type', 'created_at']
+    list_filter = ['category', 'user_type']
+    search_fields = ['title', 'description']
