bc7001ef55599c7a5f79531384f67e71c0507d02
Kshitij Moghe <kshitijmoghe10@gmail.com>
2025-03-13 10:43:47 +0530

    serializers updated

--- catogories/serializers.py
+++ catogories/serializers.py
@@ -5,3 +5,10 @@ class FormSerializer(serializers.ModelSerializer):
     class Meta:
         model = Form
         fields = '__all__'
+    def get_tech_stack_list(self, obj):
+        """Returns tech stack as a list for the API"""
+        return obj.get_tech_stack_list()
+    
+    def get_achievements_list(self, obj):
+        """Returns achievements as a list for the API"""
+        return obj.get_achievements_list()
