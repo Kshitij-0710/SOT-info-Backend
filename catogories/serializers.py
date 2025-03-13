fc95738ab90dace0d98d395b68e66a528cfeb8f1
Kshitij Moghe <kshitijmoghe10@gmail.com>
2025-03-13 11:46:08 +0530

    fixed serializers

--- catogories/serializers.py
+++ catogories/serializers.py
@@ -4,7 +4,12 @@ from .models import Form
 class FormSerializer(serializers.ModelSerializer):
     class Meta:
         model = Form
-        fields = '__all__'
+        fields = fields = [
+            'id', 'title', 'description', 'team_members', 
+            'tech_stack', 'tech_stack', 'projecturl', 
+            'achivements', 'achivements', 'from_date', 
+            'to_date', 'category', 'created_at', 'user_type'
+        ]
     def get_tech_stack_list(self, obj):
         """Returns tech stack as a list for the API"""
         return obj.get_tech_stack_list()
