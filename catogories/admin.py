6ec0c2d32aaaf15989d464bc50126d73241f7983
Kshitij Moghe <kshitijmoghe10@gmail.com>
2025-03-17 01:28:57 +0530

    is top 6 filter added

--- catogories/admin.py
+++ catogories/admin.py
@@ -3,6 +3,6 @@ from .models import Form
 
 @admin.register(Form)
 class FormAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category', 'user_type', 'created_at']
-    list_filter = ['category', 'user_type']
-    search_fields = ['title', 'description']
+    list_display = ['title', 'category', 'user_type', 'created_at', 'is_top_6']
+    list_filter = ['category', 'user_type', 'is_top_6']
+    search_fields = ['title', 'description', 'category', 'achivements']
