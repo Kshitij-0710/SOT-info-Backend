b3eba528526193ab03cc11220cf7c5378debc516
SajayR <techrend08@gmail.com>
2025-03-19 14:46:06 +0530

    time for access increased

--- sotinfo/settings.py
+++ sotinfo/settings.py
@@ -71,8 +71,8 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=48),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),  
 }
 load_dotenv()
 # Email settings (Using Gmail example)
