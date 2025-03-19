2e68b33c9a4170a71a2c191f81f34dc3928d7aaa
SajayR <techrend08@gmail.com>
2025-03-19 14:45:22 +0530

    added ongoing firld

--- catogories/admin.py
+++ catogories/admin.py
@@ -3,6 +3,6 @@ from .models import Form
 
 @admin.register(Form)
 class FormAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category', 'user_type', 'created_at', 'is_top_6']
-    list_filter = ['category', 'user_type', 'is_top_6']
+    list_display = ['title', 'category', 'user_type', 'created_at', 'is_top_6','is_ongoing']
+    list_filter = ['category', 'user_type', 'is_top_6','is_ongoing']
     search_fields = ['title', 'description', 'category', 'achivements']
