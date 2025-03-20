60ca2e25b83766820c80bd2107627263bd7a1198
SajayR <techrend08@gmail.com>
2025-03-21 00:42:29 +0530

    admin panrl updated for placements

--- catogories/admin.py
+++ catogories/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Form
+from .models import Form,Placement
 
 @admin.register(Form)
 class FormAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'user_type', 'created_at', 'is_top_6','is_ongoing']
     list_filter = ['category', 'user_type', 'is_top_6','is_ongoing']
     search_fields = ['title', 'description', 'category', 'achivements']
+
+
+@admin.register(Placement)
+class PlacementAdmin(admin.ModelAdmin):
+    list_display = ['title', 'student', 'company', 'package', 'date']
+    list_filter = ['company', 'date']
+    search_fields = ['title', 'description', 'company', 'student']
