55a1b216336b9c2904a91a8e57389f29b1af3d69
SajayR <techrend08@gmail.com>
2025-03-21 00:42:38 +0530

    models.py updated

--- catogories/models.py
+++ catogories/models.py
@@ -37,3 +37,15 @@ class Form(models.Model):
     def __str__(self):
         return f"{self.category.capitalize()}: {self.title}"
 
+
+class Placement(models.Model):
+    title = models.CharField(max_length=255)
+    student = models.CharField(max_length=255)  
+    description = models.TextField()
+    package = models.DecimalField(max_digits=10, decimal_places=2)  
+    company = models.CharField(max_length=255)
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.student} - {self.company} ({self.title})"
